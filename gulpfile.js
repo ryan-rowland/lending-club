15c553332fadd8d597e42c046dd8f6cf707ed2b4
Ryan Rowland <rrowland@twilio.com>
2016-02-20 00:36:40 -0800

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const eslint = require('gulp-eslint');
 const gulp = require('gulp');
 //const istanbul = require('gulp-istanbul');
 const mocha = require('gulp-mocha');
 
-gulp.task('test', () => gulp.src('./test').pipe(mocha({ reporter: 'spec' })));
+gulp.task('test', () => {
+  gulp.src('./test')
+    .pipe(mocha({ reporter: 'spec' }));
+});
+
+gulp.task('lint', () => {
+  gulp.src(['./lib/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
