262dd91b6abd6dbafe869da477ca4c1ec7e8a285
Ryan Rowland <rrowland@twilio.com>
2016-02-20 12:56:43 -0800

    Added documentation and took a second pass

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,26 @@
 
 const eslint = require('gulp-eslint');
 const gulp = require('gulp');
+const jsdoc = require('gulp-jsdoc-to-markdown');
 //const istanbul = require('gulp-istanbul');
 const mocha = require('gulp-mocha');
+const rename = require('gulp-rename');
 
 gulp.task('test', () => {
-  gulp.src('./test')
+  return gulp.src('./test')
     .pipe(mocha({ reporter: 'spec' }));
 });
 
 gulp.task('lint', () => {
-  gulp.src(['./lib/**/*.js'])
+  return gulp.src(['./lib/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
+});
+
+gulp.task('docs', () => {
+  return gulp.src(['lib/**/*.js'])
+  .pipe(jsdoc())
+  .pipe(rename(function (path) { path.extname = '.md' }))
+  .pipe(gulp.dest('./docs'));
 });
